8ebac8e6192b696c0b41acfb794413e6dc94d9ce
Max Thielen <497441@student.saxion.nl>
2023-05-11 13:58:06 +0200

    comments

--- src/main.py
+++ src/main.py
@@ -1,6 +1,8 @@
 import os
 from time import sleep
 
+from point_cloud import PointCloud
+
 
 def set_global():
     os.environ['img_path'] = "/data/img/"
@@ -12,18 +14,17 @@ def set_global():
 if __name__ == '__main__':
     set_global()
 
-    while True:
-        print("Hello00000 World!")
-        print(os.environ['img_path'])
-        sleep(10)
-
-    # pc = PointCloud('03', '-bin.pcd')
-    # pc.show()
-    # features = pc.extract_features()
-    # holes = pc.segment_image(io.imread(os.path.realpath('.') + os.getenv('prep_path') + '03-bin.png'), 1/spatial_res)
-    #
-    # print(f"Features: {features}")
-    # print(f"Holes: {holes}")
+    # while True:
+    #     print("Hello00000 World!")
+    #     print(os.environ['img_path'])
+    #     sleep(10)
+    pc = PointCloud('03', '-bin.pcd')
+    pc.show()
+    features = pc.extract_features()
+    holes = pc.segment_image(io.imread(os.path.realpath('.') + os.getenv('prep_path') + '03-bin.png'), 1/spatial_res)
+    
+    print(f"Features: {features}")
+    print(f"Holes: {holes}")
 
 
 
