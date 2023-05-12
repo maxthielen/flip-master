00eec1e73cb0cbf51a98296a9d768dfd73aab1a5
thielen <mthielen@aemics.nl>
2023-05-12 14:00:41 +0200

    add minimal publisher

--- main.py
+++ main.py
@@ -1,8 +1,27 @@
 import os
+import rclpy
+from rclpy.node import Node
+from std_msgs.msg import String
 from time import sleep
 from skimage import io
 from app.library.pcd_to_2d import spatial_res
 from app.modules.point_cloud import PointCloud
+
+class MinimalPublisher(Node):
+
+    def __init__(self):
+        super().__init__('minimal_publisher')
+        self.publisher_ = self.create_publisher(String, 'topic', 10)
+        timer_period = 0.5  # seconds
+        self.timer = self.create_timer(timer_period, self.timer_callback)
+        self.i = 0
+
+    def timer_callback(self):
+        msg = String()
+        msg.data = 'Hello World: %d' % self.i
+        self.publisher_.publish(msg)
+        self.get_logger().info('Publishing: "%s"' % msg.data)
+        self.i += 1
 
 
 def set_global():
@@ -15,10 +34,15 @@ def set_global():
 if __name__ == '__main__':
     set_global()
 
-    while True:
-        print("Hello World!")
-        print(os.environ['img_path'])
-        sleep(10)
+    rclpy.init()
+
+    node = MinimalPublisher()
+    try:
+        rclpy.spin(node)
+    except KeyboardInterrupt:
+        pass
+    node.destroy_node()
+    rclpy.shutdown()
 
     # pc = PointCloud('03', '-bin.pcd')
     # pc.show()
