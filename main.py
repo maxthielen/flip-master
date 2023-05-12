5d76f4b3aa8f5bf3bd7413db6c0674db2c9b2c94
thielen <mthielen@aemics.nl>
2023-05-12 14:15:25 +0200

    minimal service

--- main.py
+++ main.py
@@ -1,9 +1,22 @@
 import os
+import rclpy
+from rclpy.node import Node
+from srv import AddTwoInts
 from time import sleep
 from skimage import io
 from app.library.pcd_to_2d import spatial_res
 from app.modules.point_cloud import PointCloud
 
+class MinimalService(Node):
+    def __init__(self):
+        super().__init__('minimal_service')
+        self.srv = self.create_service(AddTwoInts, 'add_two_ints', self.add_two_ints_callback)
+
+    def add_two_ints_callback(self, request, response):
+        response.sum = request.a + request.b
+        self.get_logger().info('Incoming request\na: %d b: %d' % (request.a, request.b))
+
+        return response
 
 def set_global():
     os.environ['img_path'] = "/data/img/"
@@ -15,10 +28,13 @@ def set_global():
 if __name__ == '__main__':
     set_global()
 
-    while True:
-        print("Hello World!")
-        print(os.environ['img_path'])
-        sleep(10)
+    rclpy.init()
+    node = MinimalService()
+    try:
+        rclpy.spin(node)
+    except KeyboardInterrupt:
+        pass
+    rclpy.shutdown()
 
     # pc = PointCloud('03', '-bin.pcd')
     # pc.show()
